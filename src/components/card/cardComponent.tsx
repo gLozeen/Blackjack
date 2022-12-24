acd6d990fa2e7e3791af3f0820a6b7a08266e88c
Pavel Remarchuk <121202185+gLozeen@users.noreply.github.com>
2022-12-24 03:55:02 +0100

    feat: suit switch color

--- src/components/card/cardComponent.tsx
+++ src/components/card/cardComponent.tsx
@@ -17,6 +17,16 @@ export const CardComponent: React.FC<CardComponentProps> = (
 ) => (
   <div className="card">
     <div className="rank">{props.card.rank}</div>
-    <div className="suit">{getDisplaySuit(props.card.suit)}</div>
+    <div
+      className="suit"
+      style={{
+        color:
+          props.card.suit === "spades" || props.card.suit === "clubs"
+            ? "rgb(96, 137, 255)"
+            : "rgb(250, 103, 120)",
+      }}
+    >
+      {getDisplaySuit(props.card.suit)}
+    </div>
   </div>
 );
